b56c67f5560f81aad504dcf3627c24b6c23a669b
thuguerre <huguerre.thomas@gmail.com>
2019-12-07 10:43:38 +0100

    excluding desktop.ini to prevent execution fails

--- clean-download-folder.ps1
+++ clean-download-folder.ps1
@@ -6,7 +6,7 @@ $limit = (Get-Date).AddDays(-6)
 $path = "C:\Users\GraouFamily\Downloads"
 
 # Delete files older than the $limit.
-Get-ChildItem -Path $path -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item
+Get-ChildItem -Path $path -Force -exclude desktop.ini | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item
 
 # Delete any empty directories left behind after deleting the old files.
 # Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
